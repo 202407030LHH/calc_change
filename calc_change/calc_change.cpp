4dc50908c9cd82b231c181fe4d06e113058b9d13
202407030LHH <super8114@yuhan.ac.kr>
2025-06-08 01:51:04 +0900

    1. dev/main [ * ] main data modifined

--- calc_change/calc_change.cpp
+++ calc_change/calc_change.cpp
@@ -2,7 +2,16 @@
 #include "option.h"
 using namespace std;
 
-int main()
-{
-    
+int main() {
+	int x = 0;
+	int y = 0;
+
+	cout << "첫번째 수를 입력하세요 : ";
+	cin >> x;
+	cout << "두번째 수를 입력하세요 : ";
+	cin >> y;
+
+	printf("입력된 수 x : [ %d ] y : [ %d ]\n", x, y);
+	printf("add [ %d ] sub [ %d ] mul [ %d ] div [ %d ] mod [ %d ]\n", add(x, y), sub(x, y), mul(x, y), divd(x, y), mod(x, y));
+	return 0;
 }
